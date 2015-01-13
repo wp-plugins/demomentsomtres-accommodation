902bf491ab1048be82d68fb99b833cae592af834
marcqueralt <marcqueralt@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-01-13 13:02:30 +0000

    V1.2.2
    git-svn-id: https://plugins.svn.wordpress.org/demomentsomtres-accommodation/tags/1.2.1@1067015 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ demomentsomtres-accommodation.php
@@ -0,0 +1,37 @@
+<?php
+
+/*
+  Plugin Name: DeMomentSomTres Accommodation
+  Plugin URI: http://demomentsomtres.com/english/wordpress-plugins/demomentsomtres-accommodation/
+  Description: Accommodation management
+  Version: 1.2.1
+  Author: Marc Queralt
+  Author URI: http://demomentsomtres.com
+ */
+
+// Make sure we don't expose any info if called directly
+if (!function_exists('add_action')) {
+    echo "Hi there!  I'm just a plugin, not much I can do when called directly.";
+    exit;
+}
+
+define('DMST_ACCOMMODATION_PLUGIN_URL', plugin_dir_url(__FILE__));
+define('DMST_ACCOMMODATION_PLUGIN_PATH', plugin_dir_path(__FILE__));
+define('DMST_ACCOMMODATIO_LANG_DIR', dirname(plugin_basename(__FILE__)) . '/languages');
+define('DMST_ACCOMMODATION_TEXT_DOMAIN', 'DeMomentSomTres-Accommodation');
+define('DMST_ACCOMMODATION_OPTIONS', 'dmst_accommodation');
+// @deprecated since version 1.0
+//define('DMST_ACCOMMODATION_DBVER','20131211'); 
+
+require_once DMST_ACCOMMODATION_PLUGIN_PATH . 'demomentsomtres-class.php';
+require_once DMST_ACCOMMODATION_PLUGIN_PATH . 'demomentsomtres-functions.php';
+require_once DMST_ACCOMMODATION_PLUGIN_PATH . 'demomentsomtres-widgets.php';
+if (is_admin()):
+    require_once DMST_ACCOMMODATION_PLUGIN_PATH . 'demomentsomtres-admin-helper.php';
+    require_once DMST_ACCOMMODATION_PLUGIN_PATH . 'demomentsomtres-admin.php';
+endif;
+
+if (class_exists('DeMomentSomTresAccommodation')):
+    $DeMomentSomTresAccommodation = new DeMomentSomTresAccommodation();
+endif;
+?>
---
+++ readme.txt
@@ -0,0 +1,41 @@
+=== DeMomentSomTres Acommodation ===
+Contributors: marcqueralt
+Tags: acommodation, multisite, availability
+Requires at least: 3.7
+Tested up to: 3.9
+Stable tag: head
+
+== Description ==
+
+= Features =
+* Widget: Other accommodations on same type - creates a menu textual or grafical of other accommodations in the same type.
+
+= History & Raison d’être =
+
+== Installation ==
+
+This plugin can be installed as any other WordPress plugin. 
+
+= Usage =
+
+= Requirements =
+
+== Frequently Asked Questions ==
+
+== Screenshots ==
+
+TBD
+
+== Changelog ==
+= 1.2.1 = 
+* Widget names changed to group them
+* 3.9 compatibility checked
+
+= 1.2 =
+* SEO slug for accommodation type defined
+
+= 1.1 = 
+* Bug on other accommodations widget links
+
+= 1.0 =
+* Initial version translation ready
